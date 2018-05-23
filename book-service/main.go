3343211f4e9bf344f50c96026052fac7e3298ad8
Tamara Kaufler <tkaufler@gmail.com>
2018-05-23 18:47:18 +0100

    Adds final part of authentication
    Fixes author proto
    Adds authentication handler to book service and book cli

--- book-service/main.go
+++ book-service/main.go
@@ -1,14 +1,20 @@
 package main
 
 import (
+	"errors"
 	"log"
 	"os"
 
 	micro "github.com/micro/go-micro"
+	"github.com/micro/go-micro/client"
+	"github.com/micro/go-micro/metadata"
+	"github.com/micro/go-micro/server"
 	_ "github.com/micro/go-plugins/registry/mdns"
 	k8s "github.com/micro/kubernetes/go/micro"
+	authorProto "github.com/tamarakaufler/publication-manager/author-service/proto"
 	proto "github.com/tamarakaufler/publication-manager/book-service/proto"
 	publisherProto "github.com/tamarakaufler/publication-manager/publisher-service/proto"
+	"golang.org/x/net/context"
 )
 
 const (
@@ -25,20 +31,18 @@ func main() {
 
 	session, err := CreateSession(host)
 
-	// Mgo creates a 'master' session, we need to end that session
+	// End mongo session
 	// before the main function closes.
 	defer session.Close()
 
 	if err != nil {
-
-		// We're wrapping the error returned from our CreateSession
-		// here to add some context to the error.
 		log.Panicf("Could not connect to datastore with host %s - %v", host, err)
 	}
 
 	microSrv := k8s.NewService(
 		micro.Name("publication.management.book"),
 		micro.Version("latest"),
+		micro.WrapHandler(Authenticator),
 	)
 	pubClient := publisherProto.NewPublisherServiceClient("publication.management.publisher", microSrv.Client())
 
@@ -49,5 +53,27 @@ func main() {
 	//reflection.Register(s)
 	if err := microSrv.Run(); err != nil {
 		log.Fatalf("failed to serve: %v", err)
+	}
+}
+
+func Authenticator(fn server.HandlerFunc) server.HandlerFunc {
+	return func(ctx context.Context, req server.Request, resp interface{}) error {
+		meta, ok := metadata.FromContext(ctx)
+		if !ok {
+			return errors.New("no auth meta-data found in request")
+		}
+		token := meta["Token"]
+		log.Println("Authenticating with token: ", token)
+
+		authorClient := authorProto.NewAuthorServiceClient("publication.manager.author", client.DefaultClient)
+
+		_, err := authorClient.ValidateToken(context.Background(), &authorProto.Token{
+			Token: token,
+		})
+		if err != nil {
+			return err
+		}
+		err = fn(ctx, req, resp)
+		return err
 	}
 }
