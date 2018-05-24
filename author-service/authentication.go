979f5cb208fdf53779118b732b094a0fdc88f48b
Tamara Kaufler <tkaufler@gmail.com>
2018-05-24 13:28:19 +0100

    Cleanup and minor improvements

--- author-service/authentication.go
+++ author-service/authentication.go
@@ -8,55 +8,46 @@ import (
 )
 
 var (
-
-	// Define a secure key string used
-	// as a salt when hashing our tokens.
-	// Please make your own way more secure than this,
-	// use a randomly generated md5 hash or something.
+	// Secure key string used as a salt when hashing tokens
 	key = []byte("b9d4d807c28ca2b63bc7468e350e4493")
 )
 
 // hashed and sent as the second segment of the JWT
-type Claims struct {
+type CustomClaims struct {
 	Author *proto.Author
 	jwt.StandardClaims
 }
 
 type Authentication interface {
-	Decode(token string) (*Claims, error)
+	Decode(token string) (*CustomClaims, error)
 	Encode(author *proto.Author) (string, error)
 }
 
 type TokenService struct {
 }
 
-func (ts TokenService) Decode(tokenString string) (*Claims, error) {
-
-	token, err := jwt.ParseWithClaims(tokenString, &Claims{}, func(token *jwt.Token) (interface{}, error) {
+func (ts TokenService) Decode(tokenString string) (*CustomClaims, error) {
+	token, err := jwt.ParseWithClaims(tokenString, &CustomClaims{}, func(token *jwt.Token) (interface{}, error) {
 		return key, nil
 	})
 
-	// Validate the token => return the custom claims
-	if claims, ok := token.Claims.(*Claims); ok && token.Valid {
+	// Validate the token => return custom claims
+	if claims, ok := token.Claims.(*CustomClaims); ok && token.Valid {
 		return claims, nil
-	} else {
-		return nil, err
 	}
+	return nil, err
 }
 
 func (ts TokenService) Encode(author *proto.Author) (string, error) {
-
 	expireToken := time.Now().Add(time.Hour * 48).Unix()
 
-	claims := Claims{
+	claims := CustomClaims{
 		author,
 		jwt.StandardClaims{
 			ExpiresAt: expireToken,
-			Issuer:    "publication-manager.author",
+			Issuer:    "publication.manager.author",
 		},
 	}
 	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
-
-	// Signing the token
 	return token.SignedString(key)
 }
